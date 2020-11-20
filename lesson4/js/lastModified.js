dfb2cabc6ed8dfdd037e88221e2fcdc65365a62b
Jehmel <ofr20001@byui.edu>
2020-11-20 21:07:56 +0800

    js2

--- lesson4/js/lastModified.js
+++ lesson4/js/lastModified.js
@@ -1,1 +1,5 @@
 document.getElementById('lastModified').innerHTML = `Last Updated: ${document.lastModified}`;
+
+function toggleMenu(){
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
